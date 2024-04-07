6082aeb47d4995ebbd8dd25d985eab73e8f7620b
nithishkumarmurugesan2001@gmail.com <nithishkumarmurugesan2001@gmail.com>
2024-04-07 10:08:24 +0530

    Done correct call hell function

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,4 +1,4 @@
-function count1(fun){
+function count1(count2){
     setTimeout(()=>{
         let divCount=document.createElement('div');
         divCount.setAttribute('id','count');
@@ -9,58 +9,58 @@ function count1(fun){
     },1000)
 }
 
-function count2(){
+function count2(count3){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>2</h1>`;
             console.log(2);
     },2000)
-    count3();
+    
 }
 
-function count3(){
+function count3(count4){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>3</h1>`;
             console.log(3);
     },3000)
-    count4();
+    //count4();
 }
 
-function count4(){
+function count4(count5){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>4</h1>`;
             console.log(4);
     },4000)
-    count5();
+    //count5();
 }
 
-function count5(){
+function count5(count6){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>5</h1>`;
             console.log(5);
     },5000)
-    count6();
+    //count6();
 }
 
-function count6(){
+function count6(count7){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>6</h1>`;
             console.log(6);
     },6000)
-    count7();
+    //count7();
 }
 
-function count7(){
+function count7(count8){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>7</h1>`;
             console.log(7);
     },7000)
-    count8();
+    //count8();
 }
 
 function count8(){
@@ -69,16 +69,16 @@ function count8(){
         divCount.innerHTML=`<h1>8</h1>`;
             console.log(8);
     },8000)
-    count9();
+    //count9();
 }
 
-function count9(){
+function count9(count9){
     setTimeout(()=>{
         let divCount=document.getElementById('count');
         divCount.innerHTML=`<h1>9</h1>`;
             console.log(9);
     },9000)
-    count10();
+    //count10();
 }
 function count10(){
     setTimeout(()=>{
@@ -97,6 +97,6 @@ function count10(){
 }
 
 
-count1(count2());
+count1(count2(count3(count4(count5(count6(count7(count8(count9(count10())))))))));
 
 
